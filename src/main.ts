ac9b0e53b801d8a9bec5a0416082aaa4ba85f1ba
Jang-DongHo <donghoo18@naver.com>
2024-08-30 21:47:03 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,18 +13,15 @@ import { nodeProfilingIntegration } from '@sentry/profiling-node';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const nodeEnv = process.env.NODE_ENV;
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [nodeProfilingIntegration()],
+    // Tracing
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
 
-  if (nodeEnv == 'prod') {
-    Sentry.init({
-      dsn: process.env.SENTRY_DSN,
-      integrations: [nodeProfilingIntegration()],
-      // Tracing
-      tracesSampleRate: 1.0, //  Capture 100% of the transactions
-
-      // Set sampling rate for profiling - this is relative to tracesSampleRate
-      profilesSampleRate: 1.0,
-    });
-  }
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+  });
 
   app.setGlobalPrefix('api/node');
   app.useGlobalInterceptors(
