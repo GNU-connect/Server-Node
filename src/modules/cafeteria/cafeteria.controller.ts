381da9c7f45fc159103d1cc3281e3496d580167a
Jang-DongHo <donghoo18@naver.com>
2024-09-01 13:27:08 +0900

    [Feat] 식단 영양성분 확인

---
+++ src/modules/cafeteria/cafeteria.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CafeteriaService } from './cafeteria.service';
+import { SkillPayload } from 'src/common/interfaces/request/skillPayload';
+
+@Controller('cafeteria')
+export class CafeteriaController {
+  constructor(private readonly cafeteriaService: CafeteriaService) {}
+  @Post('get/cafeteria-diet/nutritional-ingredients')
+  async getCafeteriaDietNutritionalIngredients(
+    @Body() body: SkillPayload,
+  ): Promise<any> {
+    const { clientExtra } = body.action;
+    const cafeteriaId = clientExtra['cafeteriaId'];
+    const day = clientExtra['day'];
+    const time = clientExtra['time'];
+    const template =
+      await this.cafeteriaService.getCafeteriaDietNutritionalIngredientsSimpleText(
+        cafeteriaId,
+        day,
+        time,
+      );
+  }
+}
