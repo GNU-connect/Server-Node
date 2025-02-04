25dd44bca4c8d82d92599ec1aefa212236601817
Jang-DongHo <donghoo18@naver.com>
2025-02-05 00:22:11 +0900

    [Fix] 센트리 설정

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 dotenv.config({ path: '.env.dev' });
 import * as process from 'process';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { KakaoInterceptor } from './interceptors/kakao.interceptor';
@@ -10,12 +10,14 @@ import { ResponseDTO } from './common/dto/response.dto';
 import { SentryInterceptor } from './interceptors/sentry.interceptor.js';
 import * as Sentry from '@sentry/nestjs';
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
+import { SentryFilter } from './common/filter/sentry.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const nodeEnv = process.env.NODE_ENV;
+  console.log(nodeEnv);
 
-  if (nodeEnv == 'prod') {
+  if (nodeEnv == 'prod' ) {
     Sentry.init({
       dsn: process.env.SENTRY_NODE_DSN,
       integrations: [nodeProfilingIntegration()],
@@ -27,11 +29,14 @@ async function bootstrap() {
     )
   }
 
-  app.setGlobalPrefix('api/node');
   app.useGlobalInterceptors(
     new KakaoInterceptor(ResponseDTO)
   );
 
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new SentryFilter(httpAdapter));
+  
+  app.setGlobalPrefix('api/node');
   const config = new DocumentBuilder()
     .setTitle('커넥트지누 노드 서버 API')
     .setDescription('커넥트지누 노드 서버 API 문서입니다.')
