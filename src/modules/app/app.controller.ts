0647971e231e7b8240169631d592b398dd3b2e26
Jang-DongHo <donghoo18@naver.com>
2025-03-21 01:30:12 +0900

    refactor: WarmupService 삭제

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ResponseDTO } from '../common/dtos/response.dto';
 
 @Controller()
 export class AppController {
