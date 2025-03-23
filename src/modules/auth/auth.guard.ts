0fe078bbca72c358e154516d7c24dcc05e6afacb
Jang-DongHo <donghoo18@naver.com>
2025-03-23 20:52:20 +0900

    feat: blue-green 무중단 배포 설정

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -3,9 +3,19 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    const userId = request.headers['x-user-id'] || request.body.userRequest.user?.id;
+    const path = request.url;
+
+    // health check 엔드포인트는 인증 제외
+    if (path === '/api/node/health') {
+      return true;
+    }
+
+    const userId =
+      request.headers['x-user-id'] || request.body.userRequest.user?.id;
     if (userId) {
       request['userId'] = userId;
     }
