6486559094379f7e0f18f913b4bd683fa7e86669
Jang-DongHo <donghoo18@naver.com>
2025-04-19 11:34:10 +0900

    fix: pg pool 디버깅을 위한 설정값 변경

--- src/common/database/database.module.ts
+++ src/common/database/database.module.ts
@@ -21,12 +21,13 @@ import {
         database: configService.get('DB_DATABASE'),
         synchronize: false,
         entities: [path.join(__dirname, '../../**/*.entity{.ts,.js}')],
-        logging: process.env.NODE_ENV === 'development',
+        logging: 'all',
+        logger: 'advanced-console',
         poolSize: process.env.NODE_ENV === 'production' ? 5 : 1, // 풀 사이즈 조절
+        maxQueryExecutionTime: 3000, // 롱 쿼리 로그 출력 시간
         extra: {
-          max: 20, // 최대 연결 수
-          idleTimeoutMillis: 30000, // 유휴 연결 타임아웃
-          connectionTimeoutMillis: 4000, // 연결 타임아웃
+          max: 10, // 최대 연결 수
+          connectionTimeoutMillis: 4000, // 최대 커넥션 대기 시간
         },
       }),
       async dataSourceFactory(options) {
