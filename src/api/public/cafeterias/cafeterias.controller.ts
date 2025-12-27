7c4121e5f0f9fad3a570150c2ae39eb747be5f95
Jang-DongHo <donghoo18@naver.com>
2025-12-27 16:16:28 +0900

    ♻️ refactor: 식당 컨트롤러와 서비스 템플릿 로직 분리
    - 식당 리스트 조회 로직을 서비스로 이동
    - 식단 템플릿 생성 로직을 서비스 메서드로 분리
    - 컨트롤러의 비즈니스 로직 제거

--- src/api/public/cafeterias/cafeterias.controller.ts
+++ src/api/public/cafeterias/cafeterias.controller.ts
@@ -3,15 +3,8 @@ import { ApiTags } from '@nestjs/swagger';
 import { ApiSkillBody } from 'src/api/common/decorators/api-skill-body.decorator';
 import { ClientExtra } from 'src/api/common/decorators/skill-extra.decorator';
 import { ResponseDTO } from 'src/api/common/dtos/response.dto';
-import { BlockId } from 'src/api/common/utils/constants';
-import { CafeteriaMessagesService } from 'src/api/public/cafeterias/cafeteria-messages.service';
 import { ListCafeteriaDietExtraRequestDto } from 'src/api/public/cafeterias/dtos/requests/list-cafeteria-diet-request.dto';
 import { ListCafeteriaRequestDto } from 'src/api/public/cafeterias/dtos/requests/list-cafeteria-request.dto';
-import {
-  getDietTime,
-  getTodayOrTomorrow,
-} from 'src/api/public/cafeterias/utils/time';
-import { CampusesService } from 'src/api/public/campuses/campuses.service';
 import { CurrentUser } from 'src/api/public/users/decorators/current-user.decorator';
 import { User } from 'src/type-orm/entities/users/users.entity';
 import { CafeteriasService } from './cafeterias.service';
@@ -19,11 +12,7 @@ import { CafeteriasService } from './cafeterias.service';
 @ApiTags('cafeterias')
 @Controller('cafeterias')
 export class CafeteriasController {
-  constructor(
-    private readonly cafeteriasService: CafeteriasService,
-    private readonly campusesService: CampusesService,
-    private readonly cafeteriaMessagesService: CafeteriaMessagesService,
-  ) {}
+  constructor(private readonly cafeteriasService: CafeteriasService) {}
 
   @Post()
   @ApiSkillBody(ListCafeteriaRequestDto)
@@ -31,34 +20,12 @@ export class CafeteriasController {
     @CurrentUser() user: User,
     @ClientExtra(ListCafeteriaRequestDto) extra: ListCafeteriaRequestDto,
   ) {
-    const userCampusId = user.campus?.id;
-    const { campusId: requestedCampusId } = extra;
-
-    let template = null;
-
-    // '더보기' 버튼을 누른 경우
-    if (requestedCampusId === -1) {
-      template = await this.campusesService.campusesListCard(
-        BlockId.CAFETERIA_LIST,
-      );
-      return new ResponseDTO(template);
-    }
-
-    // 캠퍼스 ID 결정: 요청된 캠퍼스 ID 우선, 없으면 사용자 캠퍼스 ID
-    const campusId = requestedCampusId ?? userCampusId;
-
-    // 캠퍼스 ID가 없으면 캠퍼스 선택 카드 반환
-    if (!campusId) {
-      template = await this.campusesService.campusesListCard(
-        BlockId.CAFETERIA_LIST,
-      );
-      return new ResponseDTO(template);
-    }
-
-    const cafeterias = await this.cafeteriasService.getCafeterias(
-      requestedCampusId,
+    const { campusId } = extra;
+  
+    const template = await this.cafeteriasService.getCafeteriaListTemplate(
+      user.campus?.id,
+      campusId,
     );
-    template = this.cafeteriaMessagesService.cafeteriasListCard(cafeterias);
 
     return new ResponseDTO(template);
   }
@@ -69,31 +36,12 @@ export class CafeteriasController {
     @ClientExtra(ListCafeteriaDietExtraRequestDto)
     extra: ListCafeteriaDietExtraRequestDto,
   ) {
-    const { cafeteriaId } = extra;
+    const { cafeteriaId, date, time } = extra;
 
-    if (!extra.date) {
-      extra.date = getTodayOrTomorrow();
-    }
-
-    const date =
-      extra.date === '오늘'
-        ? new Date()
-        : new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
-
-    const time = extra.time ?? getDietTime(date);
-
-    const cafeteria = await this.cafeteriasService.getCafeteria(cafeteriaId);
-    const diets = await this.cafeteriasService.getCafeteriaDiets(
+    const template = await this.cafeteriasService.getCafeteriaDietTemplate(
       cafeteriaId,
       date,
       time,
-    );
-
-    const template = this.cafeteriaMessagesService.cafeteriaDietsListCard(
-      cafeteria,
-      date,
-      time,
-      diets,
     );
 
     return new ResponseDTO(template);
--- src/api/public/cafeterias/cafeterias.service.ts
+++ src/api/public/cafeterias/cafeterias.service.ts
@@ -1,30 +1,76 @@
 import { Injectable } from '@nestjs/common';
-import { DietTime } from 'src/api/public/cafeterias/dtos/requests/list-cafeteria-diet-request.dto';
-import { CafeteriaDiet } from 'src/type-orm/entities/cafeterias/cafeteria-diet.entity';
-import { Cafeteria } from 'src/type-orm/entities/cafeterias/cafeteria.entity';
+import { SkillTemplate } from 'src/api/common/interfaces/response/fields/template';
+import { BlockId } from 'src/api/common/utils/constants';
+import { CafeteriaMessagesService } from 'src/api/public/cafeterias/cafeteria-messages.service';
+import {
+  DietDate,
+  DietTime,
+} from 'src/api/public/cafeterias/dtos/requests/list-cafeteria-diet-request.dto';
+import {
+  getDietTime,
+  getTodayOrTomorrow,
+} from 'src/api/public/cafeterias/utils/time';
+import { CampusesService } from 'src/api/public/campuses/campuses.service';
 import { CafeteriasRepository } from 'src/type-orm/entities/cafeterias/cafeterias.repository';
 
 @Injectable()
 export class CafeteriasService {
-  constructor(private readonly cafeteriasRepository: CafeteriasRepository) {}
+  constructor(
+    private readonly cafeteriasRepository: CafeteriasRepository,
+    private readonly campusesService: CampusesService,
+    private readonly cafeteriaMessagesService: CafeteriaMessagesService,
+  ) {}
 
-  public async getCafeterias(campusId: number): Promise<Cafeteria[]> {
-    return this.cafeteriasRepository.findCafeteriasByCampusId(campusId);
+  /**
+   * 식당 목록 템플릿 조회
+   */
+  public async getCafeteriaListTemplate(
+    userCampusId?: number,
+    requestedCampusId?: number,
+  ): Promise<SkillTemplate> {
+    // 1. '더보기' 버튼을 누른 경우 캠퍼스 선택 카드 반환
+    if (requestedCampusId === -1) {
+      return this.campusesService.campusesListCard(BlockId.CAFETERIA_LIST);
+    }
+
+    // 2. 캠퍼스 ID 결정: 요청된 캠퍼스 ID 우선, 없으면 사용자 캠퍼스 ID 사용
+    const campusId = requestedCampusId ?? userCampusId;
+
+    // 3. 캠퍼스 ID가 없으면 캠퍼스 선택 카드 반환 (비로그인 유저 최초 진입)
+    if (!campusId) {
+      return this.campusesService.campusesListCard(BlockId.CAFETERIA_LIST);
+    }
+
+    // 4. 식당 목록 조회
+    const cafeterias = await this.cafeteriasRepository.findCafeteriasByCampusId(campusId);
+
+    // 5. 식당 목록 카드 생성
+    return this.cafeteriaMessagesService.cafeteriasListCard(cafeterias);
   }
 
-  public async getCafeteria(cafeteriaId: number): Promise<Cafeteria> {
-    return this.cafeteriasRepository.findCafeteriaById(cafeteriaId);
-  }
-
-  public async getCafeteriaDiets(
+  /**
+   * 식당 식단 템플릿 조회
+   */
+  public async getCafeteriaDietTemplate(
     cafeteriaId: number,
-    date: Date,
-    time: DietTime,
-  ): Promise<CafeteriaDiet[]> {
-    return this.cafeteriasRepository.findCafeteriaDietsByCafeteriaId(
-      cafeteriaId,
+    dietDate?: DietDate,
+    dietTime?: DietTime,
+  ): Promise<SkillTemplate> {
+    // 1. 날짜 및 시간 기본값 설정 (시간대에 따라 오늘 또는 내일 날짜 반환)
+    const date = getTodayOrTomorrow(dietDate);
+    const time = dietTime ?? getDietTime(date);
+
+    // 2. 식단 목록 조회
+    const cafeteria =
+      await this.cafeteriasRepository.findCafeteriaById(cafeteriaId);
+    const diets = await this.cafeteriasRepository.findCafeteriaDietsByCafeteriaId(cafeteriaId, date, time);
+
+    // 3. 식단 카드 생성
+    return this.cafeteriaMessagesService.cafeteriaDietsListCard(
+      cafeteria,
       date,
       time,
+      diets,
     );
   }
 }
