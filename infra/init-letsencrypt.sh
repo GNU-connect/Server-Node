d0e4fc952cdce67d1e59bc13c235c5c1dfb6d057
Jang-DongHo <donghoo18@naver.com>
2026-02-26 12:46:15 +0900

    🐛 fix: 코드래빗 리뷰 반영

--- infra/init-letsencrypt.sh
+++ infra/init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains="connectgnu.kro.kr"
 rsa_key_size=4096
-data_path="./certbot"
+data_path="./infra/certbot"
 email="connectgnu2024@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
