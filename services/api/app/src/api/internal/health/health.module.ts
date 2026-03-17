eedd622c4492dcefd21baf12446c861feb929853
Jang-DongHo <donghoo18@naver.com>
2026-03-17 23:34:51 +0900

    🔥 remove: 사용하지 않는 헬스체크 코드 제거

--- services/api/app/src/api/internal/health/health.module.ts
+++ services/api/app/src/api/internal/health/health.module.ts
@@ -1,16 +1,7 @@
 import { Module } from '@nestjs/common';
-import { makeGaugeProvider } from '@willsoto/nestjs-prometheus';
 import { HealthController } from './health.controller';
-import { HealthService } from './health.service';
 
 @Module({
   controllers: [HealthController],
-  providers: [
-    HealthService,
-    makeGaugeProvider({
-      name: 'db_connection_up',
-      help: 'Database connection status (1 = up, 0 = down)',
-    }),
-  ],
 })
 export class HealthModule {}
