408ef03cb1cad11405968a1d593e5ad480a572fc
Jang-DongHo <donghoo18@naver.com>
2026-05-01 22:26:13 +0900

    🔧 chore: Swagger API 문서 경로 및 Nginx 설정 변경

--- services/api/app/src/main.ts
+++ services/api/app/src/main.ts
@@ -52,7 +52,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
+  SwaggerModule.setup('api/api-docs', app, document, {
     swaggerOptions: {
       persistAuthorization: true,
       defaultModelsExpandDepth: -1,
